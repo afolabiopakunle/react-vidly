a452c7009b459882522197340c0897648bfb8646
Afolabi <opakunleafolabi@gmail.com>
2022-09-01 13:26:22 +0100

    movie list rendering

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
+import {getMovies} from "../services/fakeMovieService";
 
 class Movies extends Component {
     state = {
-
+        movies: getMovies()
     }
+
     render() {
         return (
-          <table className='table table-striped'>
-
-          </table>
+            <table className='table table-striped'>
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+                </thead>
+                <tbody>
+                {this.state.movies.map((movie) => (
+                    <tr key={movie._id}>
+                        <td>{movie.title}</td>
+                        <td>{movie.genre.name}</td>
+                        <td>{movie.numberInStock}</td>
+                        <td>{movie.dailyRentalRate}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         );
     }
 }
