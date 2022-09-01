be6204031aca6fd2b82625d03cc821e122f00a2b
Afolabi <opakunleafolabi@gmail.com>
2022-09-01 14:38:42 +0100

    done with deleting movies

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,8 +6,19 @@ class Movies extends Component {
         movies: getMovies()
     }
 
+    deleteMovie = (movieInput) => {
+        const movies = this.state.movies.filter(movie => movie._id != movieInput._id);
+        this.setState({movies})
+    }
+
+
     render() {
+        const {length: count} = this.state.movies;
+
+        if(count === 0) return <p className='fw-bold text-center'>There are no movies to show</p>
         return (
+            <>
+                <p className='fw-bold'>Showing {count} movies</p>
             <table className='table table-striped'>
                 <thead>
                 <tr>
@@ -24,10 +35,12 @@ class Movies extends Component {
                         <td>{movie.genre.name}</td>
                         <td>{movie.numberInStock}</td>
                         <td>{movie.dailyRentalRate}</td>
+                        <td><button onClick={() => this.deleteMovie(movie)} className="btn btn-sm btn-danger">Delete</button></td>
                     </tr>
                 ))}
                 </tbody>
             </table>
+                </>
         );
     }
 }
