a84f2f92c7c958a0c9d499ce7152a84d30651bbe
Afolabi <opakunleafolabi@gmail.com>
2022-09-01 12:28:05 +0100

    movies component created

---
+++ src/components/movies.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Movies extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+          <table className='table table-striped'>
+
+          </table>
+        );
+    }
+}
+
+export default Movies;
